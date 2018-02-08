45e66e736e8ad6981ed03071128c87ffaaea3e79
Adam Towers <hello@adamtowers.design>
2018-02-08 14:41:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return delete playlist[artistName] = songTitle;
+  return delete playlist[artistName];
 }
